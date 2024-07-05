18b5cce752062cac443e3e4e96be04cea14c2e25
saeed madi <saeed221373@yahoo.com>
2024-07-05 15:54:43 +0330

    Initial Commit

---
+++ HeartControl Watch App/HeartControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartControlApp.swift
+//  HeartControl Watch App
+//
+//  Created by saeid on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartControl_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
